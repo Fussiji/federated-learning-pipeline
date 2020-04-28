659a6869c71ff8f8fdf3c392cba498d79dfe387d
Susanna D'Souza <susanna.r.dsouza@gmail.com>
2020-04-27 20:35:28 -0700

    first push of pipeline and MNIST support code

---
+++ fed_train.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+import torch
+import time
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+import numpy as np
+import threading
+import multiprocessing
+
+import syft as sy 
+hook = sy.TorchHook(torch)
+  
+from mnist.mnist_settings import MNIST_Settings
+from mnist.mnist_settings import Net
+
+args = MNIST_Settings()
+
+# dev settings
+use_cuda = not args.no_cuda and torch.cuda.is_available()
+torch.manual_seed(args.seed)
+device = torch.device("cuda" if use_cuda else "cpu")
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+# initialize virtual workers
+vworker_comp = []
+
+for worker in range(args.vworkers):
+  id_string = "worker" + str(worker)
+  vworker_comp.append(sy.VirtualWorker(hook, id=id_string))
+
+# initialize train and test dataset
+train_dataset, test_dataset = args.gen_data()
+
+train_loader = torch.utils.data.DataLoader(
+    train_dataset,
+    batch_size=args.batch_size, shuffle=True, **kwargs)
+
+test_loader = torch.utils.data.DataLoader(
+    test_dataset,
+    batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+# federate dataset
+remote_dataset = []
+for worker in range(args.vworkers):
+  remote_dataset.append(list())
+
+remote_dataset = args.datafed_method(remote_dataset, train_loader, vworker_comp)
+
+# performs a round of training on local worker if available
+def update(args, model, device, data, target, optimizer):
+    model.train()
+    model.send(data.location)
+    data, target = data.to(device), target.to(device)
+    optimizer.zero_grad()
+    output = model(data)
+    loss = args.loss(output, target)
+    loss.backward()
+    optimizer.step()
+    model.get()
+    return model, loss
+
+def train_fn (model, device, data, target, vworker_optimizers, epoch):
+    # update remote models
+    worker_idx = int(data.location.id[-1])      
+    vworker_models[worker_idx], loss = update(args, model, device, data, target, vworker_optimizers[worker_idx])
+    return
+
+def train(args, vworker_models, device, remote_dataset, vworker_optimizers, vworker_avail, epoch):
+    print('{} batches'.format(len(remote_dataset[0])-1))
+    
+    for data_index in range(len(remote_dataset[0])-1): # batch
+       
+        for i in range(args.vworkers): # worker
+            if(vworker_avail[i] < 0.5): # non-availability
+                #print("worker " + str(i) + " not available")
+                continue # don't calculate gradient update for this data
+                        
+            data, target = remote_dataset[i][data_index]
+            train_fn(vworker_models[i], device, data, target, vworker_optimizers, epoch)
+        
+        if data_index % args.log_interval == 0:
+            print('batch {} complete'.format(data_index))
+
+# test code
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += args.loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.argmax(1, keepdim=True) # get the index of the max log-probability 
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+# create remote models, parameters, optimizers
+vworker_models, vworker_params, vworker_optimizers = args.gen_local(device)
+
+# training pipeline
+start = time.time()
+
+for epoch in range(1, args.epochs + 1):
+    vworker_avail = np.random.choice(2, args.vworkers, p=[1-args.p_available, args.p_available])
+    print('Epoch {} Availability:\n{}'.format(epoch, vworker_avail))
+
+    for i in range(args.local_epochs):
+        train(args, vworker_models, device, remote_dataset, vworker_optimizers, vworker_avail, epoch) # train
+    vworker_models, vworker_params = args.aggregate(vworker_models, device, vworker_params, vworker_avail) # aggregate
+    test(args, vworker_models[0], device, test_loader) # test
+
+end = time.time()
+print('Time elapsed: {} sec'.format(end - start))
+
+
+if (args.save_model):
+    torch.save(model.state_dict(), "mnist_cnn.pt")
---
+++ mnist/mnist_settings.py
@@ -0,0 +1,115 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+import copy
+import numpy as np
+
+import syft as sy
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+class MNIST_Settings():
+    def __init__(self):
+        # local training
+        self.batch_size = 16
+        self.lr = 0.01
+        self.local_epochs = 1
+        self.loss = F.nll_loss
+        # pipeline
+        self.no_cuda = False
+        self.seed = 1
+        self.log_interval = 30
+        self.test_batch_size = 1000
+        self.epochs = 3
+        self.vworkers = 10
+        self.p_available = 1    # probability a virtual worker is available
+        self.save_model = False
+
+    # create datasets
+    def gen_data(self):
+        mnist_train = datasets.MNIST('../data', train=True, download=True,
+                        transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                        ]))
+
+        mnist_test = datasets.MNIST('../data', train=False, 
+                        transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                        ]))
+        
+        return mnist_train, mnist_test
+
+    # federate dataset
+    def datafed_method(self, remote_dataset, train_loader, vworker_comp):
+        for batch_idx, (data,target) in enumerate(train_loader):
+            data = data.send(vworker_comp[batch_idx % self.vworkers])
+            target = target.send(vworker_comp[batch_idx % self.vworkers])
+            remote_dataset[batch_idx % self.vworkers].append((data, target))
+
+        return remote_dataset
+
+    # aggregate local models
+    def aggregate(self, vworker_models, device, vworker_params, vworker_avail):
+        new_params = list() # model average
+
+        for param_i in range(len(vworker_params[0])):
+            spdz_params = list()
+
+            for remote_index in range(self.vworkers):
+                if vworker_avail[remote_index] >= 0.5: # available
+                    copy_of_parameter = vworker_params[remote_index][param_i].copy()
+                    spdz_params.append(copy_of_parameter)
+
+            new_param = sum(spdz_params) / len(spdz_params) # average
+            new_params.append(new_param)
+
+        # update all remote models
+        with torch.no_grad():
+            for model in vworker_params:
+                for param in model:
+                    param *= 0
+
+            for remote_index in range(self.vworkers):
+                for param_index in range(len(vworker_params[remote_index])):
+                    vworker_params[remote_index][param_index].set_(new_params[param_index])
+
+        return vworker_models, vworker_params
+
+    def gen_local(self, device):
+        vworker_models = []
+        vworker_params = []
+        vworker_optimizers = []
+
+        og_model = Net()
+
+        for worker in range(self.vworkers):
+            worker_model = copy.deepcopy(og_model).to(device)
+            vworker_models.append(worker_model)
+            vworker_params.append(list(worker_model.parameters()))
+            vworker_optimizers.append(optim.SGD(worker_model.parameters(), lr=self.lr))
+
+        return vworker_models, vworker_params, vworker_optimizers
+
+
